df36bc8288fbd8af230bb7bb7005d9151c5862ac
lalaalal <bontin0128@gmail.com>
2023-04-09 18:54:29 +0900

    Prevent jumping when stun.

--- src/main/java/com/lalaalal/yummy/effect/Stun.java
+++ src/main/java/com/lalaalal/yummy/effect/Stun.java
@@ -2,12 +2,30 @@ package com.lalaalal.yummy.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 
 public class Stun extends MobEffect {
     protected Stun() {
         super(MobEffectCategory.HARMFUL, 0xFFFF00);
         addAttributeModifier(Attributes.MOVEMENT_SPEED, "E21757F0-D698-11ED-AFA1-0242AC120002", -15, AttributeModifier.Operation.MULTIPLY_TOTAL);
+    }
+
+    @Override
+    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+        Level level = pLivingEntity.getLevel();
+        BlockState blockState = level.getBlockState(pLivingEntity.getOnPos());
+        if (!blockState.is(Blocks.AIR))
+            pLivingEntity.makeStuckInBlock(level.getBlockState(pLivingEntity.getOnPos()), new Vec3(0.01, 0.01, 0.01));
+    }
+
+    @Override
+    public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
+        return true;
     }
 }
